f88dee96dc09c03d8b80fbb24fed902ef4c5aa71
Hoang Tran <hoang.tran.fin@gmail.com>
2022-10-24 01:33:23 +0300

    redirect if logged-in user accesses via url

--- src/Login.js
+++ src/Login.js
@@ -1,66 +1,82 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { auth } from './firebase';
-import './Login.css';
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { signInWithEmail, registerWithEmail } from "./firebase";
+import { useCurrentUser } from "./store";
+import styles from "./login.module.css";
 
 function Login() {
 
+    const [currentUser, setCurrentUser] = useCurrentUser();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
 
-    const signIn = e => {
+    console.log(currentUser);
+
+    useEffect(() => {
+        if (currentUser) {
+            navigate("/");
+        }
+    }, [currentUser]);
+
+    const handleSignIn = e => {
         e.preventDefault();
-        auth
-            .signInWithEmailAndPassword(email, password)
-            .then(auth => {
-                if (auth) {
-                    navigate('/')
-                }
-            })
+        signInWithEmail(email, password)
+            .then(auth => auth && navigate("/"))
             .catch(error => alert(error.message));
     };
 
     const register = e => {
         e.preventDefault();
-        auth
-            .createUserWithEmailAndPassword(email, password)
-            .then(auth => {
-                if (auth) {
-                    navigate('/')
-                }
-            })
+        registerWithEmail(email, password)
+            .then(auth => auth && navigate("/"))
             .catch(error => alert(error.message));
     };
 
     return (
-        <div className='login'>
-            <Link to='/'>
-                <img
-                    className='login__logo'
-                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png'
-                />
-            </Link>
+        <div className={styles.root}>
+            <div className={styles.container}>
+                <Link className={styles.logo} to="/">
+                    <img
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+                    />
+                </Link>
+                <form className={styles.form}>
+                    <h1>Sign-in</h1>
 
-            <div className='login__container'>
-                <h1>Sign-in</h1>
+                    <label>E-mail</label>
+                    <input
+                        type="text"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
 
-                <form>
-                    <h5>E-mail</h5>
-                    <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
 
-                    <h5>Password</h5>
-                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
+                    <button
+                        type="submit"
+                        onClick={handleSignIn}
+                        className={styles.button}>
+                        Continue
+                    </button>
 
-                    <button type='submit' onClick={signIn} className='login__signInButton'>Sign In</button>
+                    <button
+                        type="submit"
+                        onClick={register}
+                        className={styles["register-button"]}>
+                        Create your Amazon Account
+                    </button>
+
+                    <p>
+                        By continuing, you agree to Amazon Clone's
+                        Conditions of Use and Privacy Notice.
+                    </p>
                 </form>
-
-                <p>
-                    By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale.
-                    Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
-                </p>
-
-                <button type='submit' onClick={register} className='login__registerButton'>Create your Amazon Account</button>
             </div>
         </div>
     )
